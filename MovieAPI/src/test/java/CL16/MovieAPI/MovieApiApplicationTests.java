9c8e02eb3f7ad777df1bc1edfdb40ffccfbb22cc
cl16 <61947934+cl16@users.noreply.github.com>
2024-06-01 22:37:56 -0400

    adds spring boot project directory

---
+++ MovieAPI/src/test/java/CL16/MovieAPI/MovieApiApplicationTests.java
@@ -0,0 +1,13 @@
+package CL16.MovieAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
