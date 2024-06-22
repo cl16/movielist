976cd94fe32867f21c215c7bc68aaa16ac6dc7ae
cl16 <61947934+cl16@users.noreply.github.com>
2024-06-21 21:57:57 -0400

    fixes ngrok, uses localhost only in axiosConfig.js

--- MovieClient/movie-app-v1/src/api/axiosConfig.js
+++ MovieClient/movie-app-v1/src/api/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-    baseURL:'https://3de5-2607-fea8-571c-a3f0-244a-c4a7-6021-b38d.ngrok-free.app',
+    baseURL: 'http://localhost:8080',
     headers: {"ngrok-skip-browser-warning": "true"}
 });
